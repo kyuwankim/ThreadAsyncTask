3495c8976397aeb3923af1da6e9ab58dfa5f4204
kyuwankim <lpnids@gmail.com>
2017-06-12 12:31:06 +0900

    Add AsyncTask

--- app/src/main/java/com/kyuwankim/android/threadasynctask/MainActivity.java
+++ app/src/main/java/com/kyuwankim/android/threadasynctask/MainActivity.java
@@ -1,6 +1,7 @@
 package com.kyuwankim.android.threadasynctask;
 
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     Handler handler;
     ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,13 +38,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                run();
+                runAsync();
             }
         });
 
         // 화면에 진행상태를 표시
         // 프로그래스 다이얼로그 정의
-        progressDialog = new ProgressDialog(getBaseContext());
+        progressDialog = new ProgressDialog(this);
         progressDialog.setTitle("Loading");
         progressDialog.setMessage("Looooooooding");
         progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
@@ -55,7 +57,39 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void run() {
+    private void runAsync() {
+        new AsyncTask<Void, Void, Void>() {
+
+
+            @Override
+            protected void onPreExecute() {// doInBackground가 호출되기 전에 먼저 호출된다
+                super.onPreExecute();
+                progressDialog.show();
+            }
+
+            @Override
+            protected Void doInBackground(Void... params) {// 데이터를 처리..
+                try {
+                    Thread.sleep(10000);
+                    handler.sendEmptyMessage(MainActivity.SET_DONE);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+
+            @Override
+            protected void onPostExecute(Void aVoid) { // doInBackground 호출되고 나서 호출된다
+                super.onPostExecute(aVoid);
+                // 결과값을 메인 UI에 세팅하는 로직을 여기에 작성한다
+                setDone();
+                progressDialog.dismiss();
+            }
+        }.execute();
+    }
+
+    private void runThread() {
         // 프로그래스 창을 호출
         progressDialog.show();
         CustomThread thread = new CustomThread(handler);
@@ -65,7 +99,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 class CustomThread extends Thread {
-Handler handler;
+    Handler handler;
+
     public CustomThread(Handler handler) {
         this.handler = handler;
     }
