8bab596f6caddf225a259b7150ac291e7739c1f0
kyuwankim <lpnids@gmail.com>
2017-06-12 14:07:18 +0900

    Add AsyncTask

--- app/src/main/java/com/kyuwankim/android/threadasynctask/MainActivity.java
+++ app/src/main/java/com/kyuwankim/android/threadasynctask/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -58,8 +59,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void runAsync() {
-        new AsyncTask<Void, Void, Void>() {
+        new AsyncTask<String, Integer, Float>() {
+            // 제네릭 타입 1 - doInBackground의 인자
+            // 제네릭 타입 2 - onProgressUpdate의 인자
+            // 제네릭 타입 3 - doInBackground의 리턴타입
 
+
+            @Override
+            protected void onProgressUpdate(Integer... values) {// 주기적으로 doInBackground 에서 호출이 가능한 함수
+                                                               // Progress 사용할대 퍼센트 숫자 올라갈때
+                progressDialog.setMessage("진행 : "+values[0]+"%");
+            }
 
             @Override
             protected void onPreExecute() {// doInBackground가 호출되기 전에 먼저 호출된다
@@ -67,26 +77,33 @@ public class MainActivity extends AppCompatActivity {
                 progressDialog.show();
             }
 
+
             @Override
-            protected Void doInBackground(Void... params) {// 데이터를 처리..
+            protected Float doInBackground(String... params) {// 데이터를 처리..
                 try {
-                    Thread.sleep(10000);
+                    Log.e("AsyncTask", "첫번째값 : "+params[0]);
+                    Log.e("AsyncTask", "두번째값 : "+params[1]);
+                    for(int i = 0 ; i < 10 ; i ++){
+                        publishProgress(i*10); // <- onProgressUpdate 를 주기적으로 업데이트 해주는 메소드
+                        Thread.sleep(1000);
+                    }
                     handler.sendEmptyMessage(MainActivity.SET_DONE);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                return null;
+                return 1000.4F;
             }
 
 
             @Override
-            protected void onPostExecute(Void aVoid) { // doInBackground 호출되고 나서 호출된다
-                super.onPostExecute(aVoid);
+            protected void onPostExecute(Float result) { // doInBackground 호출되고 나서 호출된다
+
+                Log.e("AsyncTask","doInBackground 결과값 : "+result);
                 // 결과값을 메인 UI에 세팅하는 로직을 여기에 작성한다
                 setDone();
                 progressDialog.dismiss();
             }
-        }.execute();
+        }.execute("11111","22222");// doInBackground를 실행
     }
 
     private void runThread() {
